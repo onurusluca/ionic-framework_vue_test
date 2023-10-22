4988e9cb412d879da487fe52ced79be691284234
onurusluca <uslucaonur@gmail.com>
2023-10-23 01:42:57 +0900

    first commit

---
+++ src/views/HomePage.vue
@@ -0,0 +1,171 @@
+<template>
+  <ion-menu content-id="main-content">
+    <ion-header>
+      <ion-toolbar>
+        <ion-buttons slot="start">
+          <ion-menu-button></ion-menu-button>
+        </ion-buttons>
+        <ion-title>Menu Content</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">This is the menu content.</ion-content>
+  </ion-menu>
+  <ion-page id="main-content">
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-buttons slot="start">
+          <ion-menu-button></ion-menu-button>
+        </ion-buttons>
+        <ion-title>Blank</ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content :fullscreen="false">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Blank</ion-title>
+        </ion-toolbar>
+      </ion-header>
+
+      <div id="container">
+        <strong>Ready to create an app?</strong>
+        <p>
+          Start with Ionic
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://ionicframework.com/docs/components"
+            >UI Components</a
+          >
+        </p>
+
+        <ion-card class="card">
+          <ion-card-header>
+            <ion-card-title>Card Title</ion-card-title>
+            <ion-card-subtitle>Card Subtitle</ion-card-subtitle>
+          </ion-card-header>
+
+          <ion-card-content>
+            Here's a small text description for the card content. Nothing more,
+            nothing less.
+          </ion-card-content>
+        </ion-card>
+
+        <img :src="imageSrc" />
+        <ion-button @click="takePhoto()">Take Photo</ion-button>
+
+        <ion-datetime></ion-datetime>
+
+        <h1>Headinggdfgdfgdgf</h1>
+        <h2>Heading 2</h2>
+        <h3>Heading 3</h3>
+        <h4>Heading 4</h4>
+        <h5>Heading 5</h5>
+        <h6>Heading 6</h6>
+
+        <p>
+          Here's a small text description for the content. Nothing more, nothing
+          less.
+        </p>
+
+        <ion-button id="open-action-sheet">Open</ion-button>
+        <ion-action-sheet
+          trigger="open-action-sheet"
+          header="Actions"
+          :buttons="actionSheetButtons"
+        ></ion-action-sheet>
+      </div>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonDatetime,
+  IonMenu,
+  IonMenuButton,
+  IonButtons,
+  IonButton,
+  IonActionSheet,
+} from "@ionic/vue";
+import { ref } from "vue";
+import { Camera, CameraResultType } from "@capacitor/camera";
+
+const actionSheetButtons = [
+  {
+    text: "Delete",
+    role: "destructive",
+    data: {
+      action: "delete",
+    },
+  },
+  {
+    text: "Share",
+    data: {
+      action: "share",
+    },
+  },
+  {
+    text: "Cancel",
+    role: "cancel",
+    data: {
+      action: "cancel",
+    },
+  },
+];
+
+const imageSrc = ref("");
+const takePhoto = async () => {
+  const image = await Camera.getPhoto({
+    quality: 90,
+    allowEditing: true,
+    resultType: CameraResultType.Uri,
+  });
+
+  imageSrc.value = image.webPath;
+};
+</script>
+
+<style scoped>
+#container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 50%;
+  transform: translateY(-50%);
+}
+
+#container strong {
+  font-size: 20px;
+  line-height: 26px;
+}
+
+#container p {
+  font-size: 16px;
+  line-height: 22px;
+
+  color: #8c8c8c;
+
+  margin: 0;
+}
+
+#container a {
+  text-decoration: none;
+}
+</style>
